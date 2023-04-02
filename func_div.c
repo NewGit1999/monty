dfc739443ce42e481a533dd62afda932849d908d
NewGit1999 <yasminebenali32@gmail.com>
2023-04-01 17:30:28 -0700

    devide

--- func_div.c
+++ func_div.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 /**
  * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
+ * @head: head
+ * @counter: counter
  * Return: nothing
 */
 void f_div(stack_t **head, unsigned int counter)
