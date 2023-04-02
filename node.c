c39de5a4bd53528220ce3b0aaaf2488599e94de7
NewGit1999 <yasminebenali32@gmail.com>
2023-04-01 17:24:00 -0700

    node

--- node.c
+++ node.c
@@ -1,24 +1,24 @@
 #include "monty.h"
 /**
  * addnode - add node to the head stack
- * @head: head of the stack
- * @n: new_value
- * Return: no return
+ * @head: head
+ * @n: value
+ * Return: nothing
 */
 void addnode(stack_t **head, int n)
 {
 
-	stack_t *new_node, *aux;
+	stack_t *new, *a;
 
-	aux = *head;
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	a = *head;
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
 	{ printf("Error\n");
 		exit(0); }
-	if (aux)
-		aux->prev = new_node;
-	new_node->n = n;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	*head = new_node;
+	if (a)
+		a->prev = new;
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
 }
